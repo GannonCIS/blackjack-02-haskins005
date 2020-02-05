26e1df7d52e807bfd5a74b515e08e7f2ecf83406
unknown <haskins005@gannon.edu>
2020-02-05 15:15:28 -0500

    deck printdeck

--- src/blackjack_02/Deck.java
+++ src/blackjack_02/Deck.java
@@ -34,4 +34,10 @@ public class Deck {
         return null; //Don't forgett to change this!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
     
+    public void printDeck(){
+        for(int i = 0; i < myCards.length; i++){
+            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT);
+        }
+    }
+    
 }
